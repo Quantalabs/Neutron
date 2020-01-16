75c1f2267d96bc4d01bf88a45cb9e532603d6c5f
Quant <55121845+Quantalabs@users.noreply.github.com>
2020-01-16 10:14:50 -0800

    start

--- Neutron.js
+++ Neutron.js
@@ -1,1 +1,21 @@
+var geti = function (id) {
+  var a = document.getElementById(id);
+  return a;
+}
+var get = function (className) {
+  var a = document.getElementsByClassName(className);
+  return a;
+}
+var getn = function (name) {
+  var a = document.getElementByName(name);
+  return a;
+}
+var gett = function (tag) {
+  var a = document.getElementByTagName(tag);
+  return a;
+}
 
+var activate = function (eventName,callFunction,name1) {
+  var clickerButton = document.getElementById(name1);
+  clickerButton.addEventListener(eventName,callFunction);
+};
