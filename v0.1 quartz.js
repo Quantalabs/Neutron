11ee333644e00b84402cc101f76aaf95f5c800cb
Quant <55121845+Quantalabs@users.noreply.github.com>
2020-02-16 21:11:03 -0800

    Update and rename v0.1 quartz to v0.1 quartz.js

--- v0.1 quartz.js
+++ v0.1 quartz.js
@@ -7,11 +7,11 @@ var get = function (className) {
   return a;
 }
 var getn = function (name) {
-  var a = document.getElementByName(name);
+  var a = document.getElementsByName(name);
   return a;
 }
 var gett = function (tag) {
-  var a = document.getElementByTagName(tag);
+  var a = document.getElementsByTagName(tag);
   return a;
 }
 
@@ -19,3 +19,22 @@ var activate = function (eventName,callFunction,name1) {
   var clickerButton = document.getElementById(name1);
   clickerButton.addEventListener(eventName,callFunction);
 };
+
+var pi = function () {
+  return Math.PI;
+}
+
+var cats = function (array, joinPoint) {
+  var b = array;
+  b = array.toString();
+  return b;
+}
+var csta = function (string, splitPoint) {
+  var b = string;
+  return b.split(splitPoint);
+}
+var join = function (joinArray, addArray) {
+  var b = addArray;
+  b = addArray.concat(joinArray);
+  return b;
+}
